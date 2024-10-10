63f81f134f8ded85dfd771eb01cf7525f1b92f5b
reafer184 <129442930+reafer184@users.noreply.github.com>
2024-10-11 01:01:18 +0300

    Create user_handlers.py

---
+++ user_handlers.py
@@ -0,0 +1,26 @@
+from aiogram import F, Router
+from aiogram.types import Message
+import openai
+
+from config import load_config
+
+
+openai.api_key = load_config().openai_token
+router: Router = Router()
+
+
+async def chat(text: str) -> dict:
+    response = await openai.Completion.acreate(
+        model="text-davinci-003",
+        prompt=text,
+        temperature=0.5,
+        max_tokens=500
+    )
+    return response["choices"][0]["text"]
+
+
+@router.message(F.content_type == 'text')
+async def process_message(message: Message):
+    """Принимает все текстовые сообщения."""
+    answer = await chat(message.text)
+    await message.reply(text=answer)
